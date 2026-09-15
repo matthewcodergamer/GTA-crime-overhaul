26bb1e9ff5e6c3842ff3678ef6582034ff56cab9
matthewcodergamer <gamerstriper@gmail.com>
2026-09-14 22:59:28 -0400

    Stage 0: harden Script Hook lifecycle stop path

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 #include <Windows.h>
 #include <main.h>
 
+#include <atomic>
+
 namespace {
 HMODULE g_module = nullptr;
+std::atomic<gco::Runtime*> g_runtime{nullptr};
 
 void ScriptMain() {
     gco::Runtime runtime;
+    g_runtime.store(&runtime, std::memory_order_release);
     runtime.run();
+    g_runtime.store(nullptr, std::memory_order_release);
 }
 } // namespace
 
@@ -21,10 +26,14 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID) {
         break;
 
     case DLL_PROCESS_DETACH:
+        if (auto* runtime = g_runtime.load(std::memory_order_acquire); runtime != nullptr) {
+            runtime->requestStop();
+        }
         if (g_module != nullptr) {
             scriptUnregister(g_module);
             g_module = nullptr;
         }
+        g_runtime.store(nullptr, std::memory_order_release);
         break;
 
     default:
