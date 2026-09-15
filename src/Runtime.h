0dcfbd9cd10da33bb29eaca4c7cd82fef7f8a3f4
matthewcodergamer <gamerstriper@gmail.com>
2026-09-14 22:25:58 -0400

    Stage 1: wire platform services into runtime

--- src/Runtime.h
+++ src/Runtime.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "Foundation.h"
+#include "platform/PlatformAdapters.h"
 
+#include <cstddef>
 #include <cstdint>
+#include <optional>
 
 namespace gco {
 
@@ -19,18 +22,26 @@ private:
     void tickFiveHz();
     void tickTwoHz();
     void tickOneHz();
+    void renderDebugOverlay();
     void shutdown();
 
     RuntimePaths paths_;
     RuntimeConfig config_;
     WorldStateStore worldState_;
+    platform::PlatformServices platform_;
 
     bool initialized_ = false;
+    bool missionSuspended_ = false;
     std::uint64_t frameCount_ = 0;
     std::uint64_t lastFiveHzMs_ = 0;
     std::uint64_t lastTwoHzMs_ = 0;
     std::uint64_t lastOneHzMs_ = 0;
     std::uint64_t lastHeartbeatMs_ = 0;
+
+    platform::MissionState missionState_{};
+    std::optional<platform::PedSnapshot> playerSnapshot_;
+    std::size_t nearbyPedCount_ = 0;
+    std::size_t nearbyVehicleCount_ = 0;
 };
 
 } // namespace gco
